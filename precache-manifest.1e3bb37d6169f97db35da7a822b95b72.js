8849b79326a525ec0bde2edc9943790658daf369
Shueny <shuen29@gmail.com>
2019-04-23 11:43:52 +0800

    add vue.config and gitignore

---
+++ precache-manifest.1e3bb37d6169f97db35da7a822b95b72.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "e3265da0b912786c641c",
+    "url": "/side-projects/js/about.69260e02.js"
+  },
+  {
+    "revision": "ba8b2155e3e35969aa0c",
+    "url": "/side-projects/css/app.4fb41298.css"
+  },
+  {
+    "revision": "ba8b2155e3e35969aa0c",
+    "url": "/side-projects/js/app.81c7cf79.js"
+  },
+  {
+    "revision": "aeed05391d8ae6909be8",
+    "url": "/side-projects/css/chunk-vendors.b89365ac.css"
+  },
+  {
+    "revision": "aeed05391d8ae6909be8",
+    "url": "/side-projects/js/chunk-vendors.edcf5b64.js"
+  },
+  {
+    "revision": "8b8800e803fc813dc80d0b20fbe2cb07",
+    "url": "/side-projects/img/vue_logo.8b8800e8.png"
+  },
+  {
+    "revision": "46cc9c0ee6cc00de68b030cee7af3b0f",
+    "url": "/side-projects/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/side-projects/robots.txt"
+  }
+];
