a8e99c51a49846979a97aee41f532a26783ce27d
Shueny <shuen29@gmail.com>
2019-04-23 12:07:52 +0800

    add todolist files

--- src/main.js
+++ src/main.js
@@ -1,15 +1,15 @@
-import Vue from 'vue';
-import App from './App.vue';
-import router from './router';
-import store from './store';
-import './registerServiceWorker';
-import 'bootstrap'; // Import js file
-import 'bootstrap/dist/css/bootstrap.min.css'; // Import css file
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import './registerServiceWorker'
+import 'bootstrap' // Import js file
+import 'bootstrap/dist/css/bootstrap.min.css' // Import css file
 
-Vue.config.productionTip = false;
+Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
-  render: h => h(App),
-}).$mount('#app');
+  render: h => h(App)
+}).$mount('#app')
